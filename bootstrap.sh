6417cb782e2b9cfed054ba3b67cfef7128710d95
Kees Klerk <kees@hoppinger.com>
2014-02-26 15:47:20 +0100

    Install vundle.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
