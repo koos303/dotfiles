7931233c4829fdf291a4ada918c69046290e91be
Kees Klerk <kees@hoppinger.com>
2014-02-26 15:47:43 +0100

    Install pathogen.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+  curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
