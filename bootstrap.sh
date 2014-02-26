87a2b2679859edb578b4f59a7153984ea34de566
Kees Klerk <kees@hoppinger.com>
2014-02-26 16:15:32 +0100

    Install vundle and pathogen.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,28 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-  mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-  curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+
+  # vim vundle
+  if [ ! -d ~/.vim/bundle ]; then
+    mkdir -p ~/.vim/bundle
+  fi
+
+  if [ ! -e ~/.vim/bundle/vundle ]; then
+    echo "Installing vundle"
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+  fi
+
+  if [ ! -d ~/.vim/autoload ]; then
+    mkdir -p ~/.vim/autoload
+  fi
+
+  if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
+    echo "Installing pathogen"
+    curl -Sso ~/.vim/autoload/pathogen.vim \
+      https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  fi
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
