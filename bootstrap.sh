4214528dfacbb6ec7aea158ab6eea3a5bb772c7a
Kees Klerk <kees@hoppinger.com>
2014-02-26 16:23:51 +0100

    Check for tmp and swap directories.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
-function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 
+function installVundle() {
   # vim vundle
   if [ ! -d ~/.vim/bundle ]; then
     mkdir -p ~/.vim/bundle
@@ -14,7 +12,9 @@ function doIt() {
     echo "Installing vundle"
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
   fi
+}
 
+function installPathogen() {
   if [ ! -d ~/.vim/autoload ]; then
     mkdir -p ~/.vim/autoload
   fi
@@ -25,6 +25,26 @@ function doIt() {
       https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
   fi
 }
+
+function checkTmpAndSwapDirs() {
+  if [ ! -d ~/.vim/tmp ]; then
+    mkdir -p ~/.vim/tmp
+  fi
+
+  if [ ! -d ~/.vim/swap ]; then
+    mkdir -p ~/.vim/swap
+  fi
+}
+
+function doIt() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+
+  checkTmpAndSwapDirs
+  installVundle
+  installPathogen
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
